37be320103fc5885dcb43e4f5aa4b806f91972c5
Swippy <58222035+ZackSlipper@users.noreply.github.com>
2023-03-15 20:46:39 +0200

    Added tool button toggle detection

---
+++ Scripts/Node/UI/Status.cs
@@ -0,0 +1,15 @@
+using Godot;
+using System;
+
+public partial class Status : Node
+{
+	public override void _Ready()
+	{
+
+	}
+
+	public override void _Process(double delta)
+	{
+
+	}
+}
--- Scripts/Node/UI/ToolButton.cs
+++ Scripts/Node/UI/ToolButton.cs
@@ -5,8 +5,11 @@ namespace Scribble;
 
 public partial class ToolButton : Button
 {
-    public override void _Pressed()
+    public override void _Toggled(bool buttonPressed)
     {
+        if (!buttonPressed)
+            return;
+
         GD.Print(Name);
     }
 }
