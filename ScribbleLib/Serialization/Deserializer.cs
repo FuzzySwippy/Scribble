29d891f298a8c663b87e35ca5f68f8cae34d8301
Swippy <58222035+ZackSlipper@users.noreply.github.com>
2024-05-25 21:49:06 +0300

    Added serialization helpers

---
+++ ScribbleLib/Serialization/Deserializer.cs
@@ -0,0 +1,122 @@
+using System;
+using System.Collections.Generic;
+
+namespace Scribble.ScribbleLib.Serialization;
+
+public class Deserializer
+{
+	public Dictionary<string, DeserializedObject> DeserializedObjects { get; } = new();
+
+	public Deserializer(byte[] data) =>
+		Deserialize(data);
+
+	private void Deserialize(byte[] data)
+	{
+		int index = 0;
+		while (index < data.Length)
+		{
+			int typeIndex = index;
+			SerializationTypes type = (SerializationTypes)data[index++];
+
+			index += ReadString(data, index, out string tag);
+
+			object value = null;
+			switch (type)
+			{
+				case SerializationTypes.Bool:
+					index += ReadBool(data, index, out bool boolValue);
+					value = boolValue;
+					break;
+				case SerializationTypes.Char:
+					index += ReadChar(data, index, out char charValue);
+					value = charValue;
+					break;
+				case SerializationTypes.Byte:
+					value = data[index++];
+					break;
+				case SerializationTypes.SByte:
+					value = (sbyte)data[index++];
+					break;
+				case SerializationTypes.Short:
+					value = BitConverter.ToInt16(data, index);
+					index += 2;
+					break;
+				case SerializationTypes.UShort:
+					value = BitConverter.ToUInt16(data, index);
+					index += 2;
+					break;
+				case SerializationTypes.Int:
+					value = BitConverter.ToInt32(data, index);
+					index += 4;
+					break;
+				case SerializationTypes.UInt:
+					value = BitConverter.ToUInt32(data, index);
+					index += 4;
+					break;
+				case SerializationTypes.Long:
+					value = BitConverter.ToInt64(data, index);
+					index += 8;
+					break;
+				case SerializationTypes.ULong:
+					value = BitConverter.ToUInt64(data, index);
+					index += 8;
+					break;
+				case SerializationTypes.Float:
+					value = BitConverter.ToSingle(data, index);
+					index += 4;
+					break;
+				case SerializationTypes.Double:
+					value = BitConverter.ToDouble(data, index);
+					index += 8;
+					break;
+				case SerializationTypes.String:
+					index += ReadString(data, index, out string stringValue);
+					value = stringValue;
+					break;
+				case SerializationTypes.ByteArray:
+					index += ReadByteArray(data, index, out byte[] byteArrayValue);
+					value = byteArrayValue;
+					break;
+				case SerializationTypes.DateTime:
+					value = new DateTime(BitConverter.ToInt64(data, index));
+					index += 8;
+					break;
+				default:
+					throw new Exception($"Unknown serialization type at index {typeIndex}");
+			}
+
+			DeserializedObjects.Add(tag, new DeserializedObject(type, value));
+		}
+	}
+
+	public int ReadBool(byte[] data, int index, out bool value)
+	{
+		value = BitConverter.ToBoolean(data, index);
+		return 1;
+	}
+
+	public int ReadChar(byte[] data, int index, out char value)
+	{
+		value = BitConverter.ToChar(data, index);
+		return 2;
+	}
+
+	/// <summary>
+	/// Reads a string from the data at the specified index.
+	/// </summary>
+	/// <returns>The string length plus 4 for the bytes used to store the string length</returns>
+	private int ReadString(byte[] data, int index, out string value)
+	{
+		int length = BitConverter.ToInt32(data, index);
+		value = System.Text.Encoding.UTF8.GetString(data, index + 4, length);
+		return 4 + length;
+	}
+
+	private int ReadByteArray(byte[] data, int index, out byte[] value)
+	{
+		int length = BitConverter.ToInt32(data, index);
+		value = new byte[length];
+		Array.Copy(data, index + 4, value, 0, length);
+		return 4 + length;
+	}
+}
---
+++ ScribbleLib/Serialization/Serializer.cs
@@ -0,0 +1,102 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Scribble.ScribbleLib.Serialization;
+
+namespace Scribble.ScribbleLib;
+
+public class Serializer
+{
+	private bool Finished { get; set; }
+	private List<byte> Data { get; } = new();
+	private HashSet<string> Tags { get; } = new();
+
+	public byte[] FinalData { get; private set; }
+
+	public byte[] Finish()
+	{
+		Finished = true;
+		FinalData = Data.ToArray();
+		return FinalData;
+	}
+
+	public void Write<T>(T item, string tag) where T : struct
+	{
+		if (Finished)
+			throw new Exception("Serializer has already been finished");
+
+		if (Tags.Contains(tag))
+			throw new Exception("Tag already exists");
+
+		switch (typeof(T))
+		{
+			case Type _ when typeof(T) == typeof(bool):
+				AddData(SerializationTypes.Bool, tag, BitConverter.GetBytes((bool)(object)item));
+				break;
+			case Type _ when typeof(T) == typeof(char):
+				AddData(SerializationTypes.Char, tag, BitConverter.GetBytes((char)(object)item));
+				break;
+			case Type _ when typeof(T) == typeof(byte):
+				AddData(SerializationTypes.Byte, tag, new byte[] { (byte)(object)item });
+				break;
+			case Type _ when typeof(T) == typeof(sbyte):
+				AddData(SerializationTypes.SByte, tag, new byte[] { (byte)(object)item });
+				break;
+			case Type _ when typeof(T) == typeof(short):
+				AddData(SerializationTypes.Short, tag, BitConverter.GetBytes((short)(object)item));
+				break;
+			case Type _ when typeof(T) == typeof(ushort):
+				AddData(SerializationTypes.UShort, tag, BitConverter.GetBytes((ushort)(object)item));
+				break;
+			case Type _ when typeof(T) == typeof(int):
+				AddData(SerializationTypes.Int, tag, BitConverter.GetBytes((int)(object)item));
+				break;
+			case Type _ when typeof(T) == typeof(uint):
+				AddData(SerializationTypes.UInt, tag, BitConverter.GetBytes((uint)(object)item));
+				break;
+			case Type _ when typeof(T) == typeof(long):
+				AddData(SerializationTypes.Long, tag, BitConverter.GetBytes((long)(object)item));
+				break;
+			case Type _ when typeof(T) == typeof(ulong):
+				AddData(SerializationTypes.ULong, tag, BitConverter.GetBytes((ulong)(object)item));
+				break;
+			case Type _ when typeof(T) == typeof(float):
+				AddData(SerializationTypes.Float, tag, BitConverter.GetBytes((float)(object)item));
+				break;
+			case Type _ when typeof(T) == typeof(double):
+				AddData(SerializationTypes.Double, tag, BitConverter.GetBytes((double)(object)item));
+				break;
+			case Type _ when typeof(T) == typeof(string):
+				AddData(SerializationTypes.String, tag, SerializeString((string)(object)item));
+				break;
+			case Type _ when typeof(T) == typeof(byte[]):
+				AddData(SerializationTypes.ByteArray, tag, SerializeByteArray((byte[])(object)item));
+				break;
+			case Type _ when typeof(T) == typeof(DateTime):
+				AddData(SerializationTypes.DateTime, tag, BitConverter.GetBytes(((DateTime)(object)item).Ticks));
+				break;
+			default:
+				throw new Exception("Unsupported type");
+		}
+	}
+
+	private void AddData(SerializationTypes type, string tag, byte[] data)
+	{
+		Data.Add((byte)type);
+		Data.AddRange(SerializeString(tag));
+		Data.AddRange(data);
+	}
+
+	private byte[] SerializeString(string str)
+	{
+		byte[] bytes = System.Text.Encoding.UTF8.GetBytes(str);
+		byte[] length = BitConverter.GetBytes(bytes.Length);
+		return length.Concat(bytes).ToArray();
+	}
+
+	private byte[] SerializeByteArray(byte[] bytes)
+	{
+		byte[] length = BitConverter.GetBytes(bytes.Length);
+		return length.Concat(bytes).ToArray();
+	}
+}
