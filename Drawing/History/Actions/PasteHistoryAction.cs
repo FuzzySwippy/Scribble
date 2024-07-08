84b33accee1c92d57a59ea26e35907d6d5d1e369
Swippy <58222035+ZackSlipper@users.noreply.github.com>
2024-07-08 21:35:38 +0300

    Fixed paste history action

--- Drawing/History/Actions/PasteHistoryAction.cs
+++ Drawing/History/Actions/PasteHistoryAction.cs
@@ -6,22 +6,25 @@ namespace Scribble.Drawing;
 
 public class PasteHistoryAction : HistoryAction
 {
-	private ulong CreatedLayerId { get; }
+	private int CreatedLayerIndex { get; }
 	private Vector2I MousePos { get; }
 	private Image Image { get; }
 
-	public PasteHistoryAction(ulong createdLayerId, Vector2I mousePos, Image image)
+	public PasteHistoryAction(int createdLayerIndex, Vector2I mousePos, Image image)
 	{
 		ActionType = HistoryActionType.Paste;
-		CreatedLayerId = createdLayerId;
+		CreatedLayerIndex = createdLayerIndex;
 		MousePos = mousePos;
 		Image = image;
 
 		HasChanges = true;
 	}
 
-	public override void Undo() =>
-		Global.Canvas.DeleteLayer(Global.Canvas.GetLayerIndex(CreatedLayerId), false);
+	public override void Undo()
+	{
+		Global.Canvas.DeleteLayer(CreatedLayerIndex, false);
+		Global.Canvas.Selection.Clear(false);
+	}
 
 	public override void Redo() =>
 		Global.Canvas.Paste(MousePos, Image);
